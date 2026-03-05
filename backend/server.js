973e2a6eaf968ea034698bb03ef33b16c2c2c034
crsanches <crsanches4@gmail.com>
2026-03-05 17:13:30 -0300

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const usuariosRoutes = require("./routes/usuarios");
 
-app.use("/", usuariosRoutes);
+const app = express();   // ✅ primeiro cria o app
+
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+
+// =============================
+// MIDDLEWARES
+// =============================
 
 app.use(cors());
 app.use(express.json());
+
+
+// =============================
+// ROTAS
+// =============================
+
 app.use(routes);
+app.use("/", usuariosRoutes);
+
+
+// =============================
+// START SERVIDOR
+// =============================
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT);
 });
-
-//app.listen(3000,"0.0.0.0",() => {
-//  console.log('Servidor rodando em http localhost:3000');
-//});
